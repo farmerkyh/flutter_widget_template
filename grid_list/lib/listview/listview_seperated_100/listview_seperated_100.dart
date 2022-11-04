55a9303a4fcb027bcd9806aa06b3d6a9a45fbd04
farmerkyh <farmerkyh@naver.com>
2022-11-04 15:17:28 +0900

    .

--- grid_list/lib/listview/listview_seperated_100/listview_seperated_100.dart
+++ grid_list/lib/listview/listview_seperated_100/listview_seperated_100.dart
@@ -1,3 +1,16 @@
+//----------------------------------------------------------------------------------------------
+//1. ListView
+//   - .separated       . 라인별 구분라인이 그린다.
+//   - separatorBuilder . 구분라인을 그릴때 option
+//                      . Divider(thickness):구분라인이 두꺼워진다고 해서 자동으로 구분간격도 늘어나지는 않는다.
+//                      . Divider(height)   : List 라인별 사이의 간격
+//   - itemCount . 해당 숫만큼 itemBuilder 를 수행 한다.
+//   - padding   . ListView와 item list 사이의 간격
+//2. Expanded
+//   - 남은 간격을 n분의1로 크기 설정
+//   - flex : Expanded 가 여러 개 일 경우 각각의 height 비율을 지정할 수 있다.
+//----------------------------------------------------------------------------------------------
+
 import "package:flutter/material.dart";
 
 class ListViewStep10 extends StatelessWidget {
@@ -14,19 +27,6 @@ class ListViewStep10 extends StatelessWidget {
         title: const Text('Listview.seperated'),
         centerTitle: true,
       ),
-
-      //----------------------------------------------------------------------------------------------
-      //1. ListView
-      //   - .separated       . 라인별 구분라인이 그린다.
-      //   - separatorBuilder . 구분라인을 그릴때 option
-      //                      . Divider(thickness):구분라인이 두꺼워진다고 해서 자동으로 구분간격도 늘어나지는 않는다.
-      //                      . Divider(height)   : List 라인별 사이의 간격
-      //   - itemCount . 해당 숫만큼 itemBuilder 를 수행 한다.
-      //   - padding   . ListView와 item list 사이의 간격
-      //2. Expanded
-      //   - 남은 간격을 n분의1로 크기 설정
-      //   - flex : Expanded 가 여러 개 일 경우 각각의 height 비율을 지정할 수 있다.
-      //----------------------------------------------------------------------------------------------
       body: Container(
         child: Column(
           children: [
@@ -56,7 +56,7 @@ class ListViewStep10 extends StatelessWidget {
             Expanded(
               flex: 1,
               child: ListView(
-                children: [Center(child: Text("하단"))],
+                children: const [Center(child: Text("하단"))],
               ),
             ),
           ],
